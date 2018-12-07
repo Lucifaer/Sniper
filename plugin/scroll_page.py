9ea18a5bd0b9201976c48b6c2e08219d718a328c
Lucifaer <l.lucifaer@gmail.com>
2018-12-07 19:24:10 +0800

    fix Issues 1

--- plugin/scroll_page.py
+++ plugin/scroll_page.py
@@ -1,17 +1,13 @@
-scroll_page_js = '''
-async () => {
-        await new Promise((resolve, reject) => {
-        let totalHeight = 0;
-        let distance = 100;
-        let timer = setInterval(() => {
-            let scrollHeight = document.body.scrollHeight
-            window.scrollBy(0, distance);
-            totalHeight += distance;
-            if(totalHeight >= scrollHeight){
-                clearInterval(timer);
-                resolve()
-            }
-        }, 100)
-    })
-}
-'''
+import asyncio
+
+
+async def scroll_page(page):
+    cur_dist = 0
+    height = await page.evaluate("() => document.body.scrollHeight")
+    while True:
+        if cur_dist < height:
+            await page.evaluate("window.scrollBy(0, 500);")
+            await asyncio.sleep(0.1)
+            cur_dist += 500
+        else:
+            break
