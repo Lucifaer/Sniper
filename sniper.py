3a325fbe6b2ad8111b22849de6c5d99f8e13b4b5
Lucifaer <l.lucifaer@gmail.com>
2018-12-07 15:23:00 +0800

    add sniper.py and logo.py which makes program run convenient

---
+++ sniper.py
@@ -0,0 +1,24 @@
+import click
+from core.logo import logo
+
+
+@click.group()
+def console():
+    pass
+
+
+@console.command('upload')
+@click.option('--tag', '-t', default='', help="tag标签：\n"
+                                              "格式：category/tags\n"
+                                              "如：tips/任意文件读取 或 ctf/web")
+@click.argument('url', required=True)
+def upload(url, tag):
+    from core.Tasks import CommonTasks
+    import asyncio
+    tasks = CommonTasks()
+    asyncio.run(tasks.create_tasks(url=url, tag=tag))
+
+
+if __name__ == '__main__':
+    click.secho(logo, fg='bright_green')
+    console()
