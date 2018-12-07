bf9fbf115f48bf0653c93e2213de220d2d3da66f
Lucifaer <l.lucifaer@gmail.com>
2018-12-07 18:34:47 +0800

    create develop

--- core/Spider.py
+++ core/Spider.py
@@ -23,17 +23,29 @@ class Spider(object):
     async def spider(self, url):
         self.log.detail_info(f"[*] Start crawl {url}")
         self.log.detail_info(f"[*] {url} started at {time.strftime('%X')}")
-        browser = await launch()
-        page = await browser.newPage()
-        await page.setViewport(self.set_view_port_option)
-        await page.goto(url, self.goto_option)
-        title = await page.title()
-        filename = await self.translate_word(title)
-        await page.evaluate(scroll_page_js)
-        pdf = await page.pdf(self.pdf_option)
-        await browser.close()
-        self.log.detail_info(f"[*] {url} finished at {time.strftime('%X')}")
-        return filename, pdf
+        # Handle Error: pyppeteer.errors.NetworkError: Protocol error Runtime.callFunctionOn: Target closed.
+        try:
+            browser = await launch({
+                'headless': True,
+                'args': ['--no-sandbox'],
+                'timeout': 60000
+            })
+            page = await browser.newPage()
+            await page.setViewport(self.set_view_port_option)
+            page.setDefaultNavigationTimeout(60000)
+            await page.goto(url, self.goto_option)
+            title = await page.title()
+            filename = await self.translate_word(title)
+            await page.evaluate(scroll_page_js)
+            pdf = await page.pdf(self.pdf_option)
+            await browser.close()
+            self.log.detail_info(f"[*] {url} finished at {time.strftime('%X')}")
+            return filename, pdf
+        except Exception as exc:
+            self.log.error_then(f"[-] Occur an unexcepted error when crawl {url}: {str(exc)}!")
+            self.log.detail_info(f"[*] {url} finished at {time.strftime('%X')}")
+        finally:
+            await browser.close()
 
     async def translate_word(self, word):
         table = {ord(f): ord(t) for f, t in zip(
