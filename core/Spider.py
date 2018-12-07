bd496d1ea7201f78956fb3ede7462493c40641ba
Lucifaer <l.lucifaer@gmail.com>
2018-12-07 19:24:10 +0800

    fix Issues 1

--- core/Spider.py
+++ core/Spider.py
@@ -1,8 +1,8 @@
 from core.MsgHandler import LogHandler
-from plugin.scroll_page import scroll_page_js
+from plugin.scroll_page import scroll_page
 from pyppeteer import launch
 import time
-import asyncio
+
 
 class Spider(object):
     def __init__(self):
@@ -24,37 +24,28 @@ class Spider(object):
         self.log.detail_info(f"[*] Start crawl {url}")
         self.log.detail_info(f"[*] {url} started at {time.strftime('%X')}")
         # Handle Error: pyppeteer.errors.NetworkError: Protocol error Runtime.callFunctionOn: Target closed.
-        browser = await launch()
-        # browser = await launch({
-        #     'args': ['--no-sandbox', '--disable-dev-shm-usage']
-        # })
-        page = await browser.newPage()
-        await page.setViewport(self.set_view_port_option)
-        await page.goto(url, self.goto_option)
+        # Issue fix #1 by C1tas
+        try:
+            browser = await launch()
+            page = await browser.newPage()
+            await page.setViewport(self.set_view_port_option)
+            await page.goto(url, self.goto_option)
+            await scroll_page(page)
+            pdf = await page.pdf(self.pdf_option)
+            title = await page.title()
+            filename = await self.translate_word(title)
+            await browser.close()
+            self.log.detail_info(f"[*] {url} finished at {time.strftime('%X')}")
+            return filename, pdf
+        except Exception as exc:
+            self.log.error_then(f"[-] Occur an unexcepted error when crawl {url}: {str(exc)}!")
+            self.log.detail_info(f"[*] {url} finished at {time.strftime('%X')}")
+        finally:
+            await browser.close()
 
-        cur_dist = 0
-        height = await page.evaluate("() => document.body.scrollHeight")
-        while True:
-            if cur_dist < height:
-                await page.evaluate("window.scrollBy(0, 500);")
-                await asyncio.sleep(0.1)
-                cur_dist += 500
-            else:
-                break
-        
-        pdf = await page.pdf(self.pdf_option)
-        title = await page.title()
-        filename = await self.translate_word(title)
-        await browser.close()
-        self.log.detail_info(f"[*] {url} finished at {time.strftime('%X')}")
-        return filename, pdf
-
-    async def translate_word(self, word):
+    @staticmethod
+    async def translate_word(word):
         table = {ord(f): ord(t) for f, t in zip(
             u'，。！？【】（）/％＃＠＆１２３４５６７８９０',
             u',.!?[]()-%#@&1234567890')}
         return word.translate(table)
-
-# if __name__ == '__main__':
-#     test = Spider()
-#     asyncio.run(test.create_spider('https://xz.aliyun.com/t/3264'))
